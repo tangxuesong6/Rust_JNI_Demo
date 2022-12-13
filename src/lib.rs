3a7e619bcb1c1cabaebe7e492e2f8bff93c25e0c
txs <tangxuesong666@gmail.com>
2022-12-13 20:29:36 +0800

    feat: get_signature_normal

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+
 use android_logger_lite as log;
 use jni::JNIEnv;
 use jni::objects::{JClass, JObject, JString, JValue};
 use jni::signature::Primitive::Void;
 use jni::signature::ReturnType;
 use jni::strings::JNIString;
-use jni::sys::{jbyteArray, jstring};
+use jni::sys::{jbyteArray, jobjectArray, jstring};
 
 #[no_mangle]
 pub extern "system" fn Java_com_jni_rust_RustNative_getStringFromRust(env: JNIEnv, _: JClass) -> jstring {
@@ -65,9 +66,9 @@ pub extern "system" fn Java_com_jni_rust_RustNative_asyncCallback(env: JNIEnv, _
     });
     rx.recv().unwrap();
 }
+
 #[no_mangle]
 pub unsafe extern fn Java_com_jni_rust_RustNative_singleton(env: JNIEnv, _: JClass) {
-
     let clz = match env.find_class("com/jni/rust/NativeSingleton") {
         Ok(class) => { class }
         Err(_) => {
@@ -75,26 +76,81 @@ pub unsafe extern fn Java_com_jni_rust_RustNative_singleton(env: JNIEnv, _: JCla
         }
     };
     let instance_method_id = match env.get_static_method_id(clz, "getInstance", "()Lcom/jni/rust/NativeSingleton;") {
-        Ok(ins) => {ins}
+        Ok(instance_method_id) => { instance_method_id }
         Err(_) => {
             panic!("can't find method NativeSingleton.getInstance");
         }
     };
     let instance = match env.call_static_method_unchecked(clz, instance_method_id, ReturnType::Object, &[]) {
-        Ok(obj) => {obj}
+        Ok(obj) => { obj }
         Err(_) => {
             panic!("can't call method getInstance");
         }
     };
     let instance_obj = JObject::from(instance.l().unwrap());
     let log_identity_hashcode = match env.get_method_id(clz, "logIdentityHashCode", "()V") {
-        Ok(get) => {get}
+        Ok(log_identity_hashcode) => { log_identity_hashcode }
         Err(_) => {
             panic!("can't call method logIdentityHashCode");
         }
     };
     env.call_method_unchecked(instance_obj, log_identity_hashcode, ReturnType::Primitive(Void), &[]).unwrap();
 }
+
+#[no_mangle]
+pub extern "system" fn Java_com_jni_rust_RustNative_getSignatureNormal(env: JNIEnv, _: JClass) -> jstring {
+    let activity_thread_clz = env.find_class("android/app/ActivityThread").unwrap();
+    let application_value = env.call_static_method(activity_thread_clz, "currentApplication", "()Landroid/app/Application;", &[]).unwrap();
+    let application = JObject::try_from(application_value).unwrap();
+
+    //packageName
+    let package_name_value = env.call_method(application, "getPackageName", "()Ljava/lang/String;", &[]).unwrap();
+    //JValue to JString
+    let pkg_name = JString::from(package_name_value.l().unwrap());
+    //JString to rust String
+    let pkg_name: String = env.get_string(pkg_name).unwrap().into();
+    log::d("sign".to_string(), format!("package name = {}", pkg_name));
+
+    //PackageManager.GET_SIGNATURES
+    let pm_signatures = JValue::from(64);
+    let package_manager = env.call_method(application, "getPackageManager", "()Landroid/content/pm/PackageManager;", &[]).unwrap();
+    let package_info = env.call_method(package_manager.l().unwrap(), "getPackageInfo", "(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;", &[package_name_value, pm_signatures]).unwrap();
+    let signatures_value = env.get_field(package_info.l().unwrap(), "signatures", "[Landroid/content/pm/Signature;").unwrap();
+
+    //JValue to JObject
+    let signature_array_obj = signatures_value.l().unwrap();
+
+    //JObject to jarray
+    let signature_array = jobjectArray::from(signature_array_obj.cast());
+
+    let signature_obj = env.get_object_array_element(signature_array, 0).unwrap();
+    let sign_value = env.call_method(signature_obj, "toByteArray", "()[B", &[]).unwrap();
+
+    let message_digest_clz = env.find_class("java/security/MessageDigest").unwrap();
+    let md5 = env.new_string("md5").unwrap();
+
+    //JString to JValue
+    let md5 = JValue::from(md5);
+    let message_digest_value = env.call_static_method(message_digest_clz, "getInstance",
+                                                      "(Ljava/lang/String;)Ljava/security/MessageDigest;", &[md5]).unwrap();
+    let _update = env.call_method(message_digest_value.l().unwrap(), "update", "([B)V", &[sign_value]).unwrap();
+    let digest_value = env.call_method(message_digest_value.l().unwrap(), "digest", "()[B", &[]).unwrap();
+
+    let digest_array = jbyteArray::from(digest_value.l().unwrap().cast());
+
+    //jarray to Vec
+    let digest_array = env.convert_byte_array(digest_array).unwrap();
+    //get hex
+    let hex_sign: String = digest_array.iter()
+        .map(|b| format!("{:02x}", b).to_string())
+        .collect::<Vec<String>>().join("");
+    log::d("sign".to_string(), format!("{}", hex_sign));
+
+    let hex_sign = JNIString::from(hex_sign);
+    let hex_sign = env.new_string(hex_sign).unwrap();
+    hex_sign.into_raw()
+}
+
 
 
 
